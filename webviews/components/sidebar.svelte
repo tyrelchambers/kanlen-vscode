ddfb9bff851ba6680f97c4339019ed3f0fa213c7
Tyrel Chambers <tychambers3@gmail.com>
2021-02-02 20:34:19 -0500

    .

---
+++ webviews/components/sidebar.svelte
@@ -0,0 +1,11 @@
+<script>
+  const token = false;
+</script>
+
+{#if !token}
+  <h1>Login</h1>
+  <input type="email" name="email" placeholder="email" />
+  <input type="password" name="password" placeholder="password" />
+
+  <button>Get token</button>
+{/if}
